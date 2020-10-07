0cb91ba606b7f3557d30cab5b6684002eb09ffcc
acronim <marinko.miskovic.hr@gmail.com>
2020-10-07 22:49:25 +0200

    Implementing login view

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get "about" => "static_pages#about"
   get "contact" => "static_pages#contact"
   get "signup" => "users#new"
+  get "login" => "sessions#new" # page for a new session (login)
+  post "login" => "sessions#create" # create a new session (login)
+  delete "logout" => "sessions#destroy" # delete a session (log out)
   # to get the routing for /users/1 to work
   resources :users
 end
